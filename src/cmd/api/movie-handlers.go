a7d42f7afec2c7b8bd4966a39613a14fedd85194
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-10 22:22:58 +0900

    add movie-api-route

---
+++ src/cmd/api/movie-handlers.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/nao-18/golang-backend-api/models"
+)
+
+func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
+	// get params from request
+	params := httprouter.ParamsFromContext(r.Context())
+
+	// convert string to number
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		app.logger.Print(errors.New("invalid id parameter"))
+	}
+
+	app.logger.Println("id is", id)
+
+	// create movie
+	movie := models.Movie{
+		ID:          id,
+		Title:       "Some movie",
+		Description: "Some description",
+		Year:        2021,
+		ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
+		Runtime:     100,
+		Rating:      5,
+		MPAARating:  "PG-13",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	// return json
+	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+}
+
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+
+}
