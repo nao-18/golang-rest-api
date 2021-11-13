d0c60de3f1339ea5135be9de1c11d04a2d070707
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-13 20:12:37 +0900

    add a sql to retrieve the movie

--- src/models/movies-db.go
+++ src/models/movies-db.go
@@ -15,8 +15,8 @@ func (m *DBModel) Get(id int) (*Movie, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	query := `select id, title, description, year, release_date, runtime, mpaa_rating,
-						created_at, updated_at from movies where id = $1
+	query := `select id, title, description, year, release_date, rating, runtime, mpaa_rating,
+				created_at, updated_at from movies where id = $1
 	`
 
 	row := m.DB.QueryRowContext(ctx, query, id)
@@ -40,8 +40,34 @@ func (m *DBModel) Get(id int) (*Movie, error) {
 	}
 
 	// get genres, if any
-	// query = ``
-	// rows, _ := m.DB.QueryRowContext(ctx, query, id)
+	query = `select
+				mg.id, mg.movie_id, mg.genre_id, g.genre_name
+			from
+				movies_genres mg
+				left join genres g on (g.id = mg.genre_id)
+			where
+				mg.movie_id = $1
+	`
+
+	rows, _ := m.DB.QueryContext(ctx, query, id)
+	defer rows.Close()
+
+	var genres []MovieGenre
+	for rows.Next() {
+		var mg MovieGenre
+		err := rows.Scan(
+			&mg.ID,
+			&mg.MovieID,
+			&mg.GenreID,
+			&mg.Genre.GenreName,
+		)
+		if err != nil {
+			return nil, err
+		}
+		genres = append(genres, mg)
+	}
+
+	movie.MovieGenre = genres
 
 	return &movie, nil
 }
