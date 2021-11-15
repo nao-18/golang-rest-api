d07d3c121c8184ef63b87689a2bf2130985e6f23
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-15 20:34:49 +0900

    add methods

--- src/cmd/api/movie-handlers.go
+++ src/cmd/api/movie-handlers.go
@@ -20,8 +20,6 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.logger.Println("id is", id)
-
 	movie, err := app.models.DB.Get(id)
 
 	// return json
@@ -43,5 +41,21 @@ func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
 		app.errorJSON(w, err)
 		return
 	}
+
+}
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) searchMovie(w http.ResponseWriter, r *http.Request) {
 
 }
