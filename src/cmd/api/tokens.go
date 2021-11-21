e64fb6dd49c9aeb7bb2540a37125fa18073a3209
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-21 21:45:09 +0900

    add tokens.go

---
+++ src/cmd/api/tokens.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/nao-18/golang-backend-api/models"
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/pascaldekloe/jwt"
+)
+
+var validUser = models.User{
+	ID:       10,
+	Email:    "me@here.com",
+	Password: "$2a$12$6SESL72gpJbSfH7z/zIqwu3twl.bcCzP6OUtNpgFOonHyu9pTOgyu",
+}
+
+type Credentials struct {
+	Username string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (app *application) Signin(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"))
+		return
+	}
+
+	hashedPassword := validUser.Password
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(creds.Password))
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"))
+		return
+	}
+
+	var claims jwt.Claims
+	claims.Subject = fmt.Sprint(validUser.ID)
+	claims.Issued = jwt.NewNumericTime(time.Now())
+	claims.NotBefore = jwt.NewNumericTime(time.Now())
+	claims.Expires = jwt.NewNumericTime(time.Now().Add(24 * time.Hour))
+	claims.Issuer = "mydomain.com"
+	claims.Audiences = []string{"mydomain.com"}
+
+	jwtBytes, err := claims.HMACSign(jwt.HS256, []byte(app.config.jwt.secret))
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"))
+		return
+	}
+
+	app.writeJSON(w, http.StatusOK, jwtBytes, "response")
+}
